4363cf72e65b18089d36caebf2b57d9bbe3efe3e
auttapon.c <auttaponcoe24@gmail.com>
2024-06-28 02:21:30 +0700

    core

--- src/app/api/auth/[route]/route.ts
+++ src/app/api/auth/[route]/route.ts
@@ -1,8 +1,113 @@
+// import { User } from "@/src/interfaces/auth.interface";
+// import { ACCESS_TOKEN_KEY } from "@/src/utils/constant";
+// import httpClient from "@/src/utils/httpClient";
+// import { cookies } from "next/headers";
+// import { NextRequest, NextResponse } from "next/server";
+
+// // ############# Routes #############
+// // GET
+// export async function GET(
+// 	req: NextRequest,
+// 	context: {
+// 		params: {
+// 			route: string;
+// 		};
+// 	}
+// ) {
+// 	const route = context.params.route;
+// 	if (route === "signout") {
+// 		return signout(req);
+// 	} else if (route === "session") {
+// 		return getSession(req);
+// 	}
+// 	return NextResponse.json({ route });
+// }
+
+// // POST
+// export const POST = async (
+// 	req: NextRequest,
+// 	context: { params: { route: string } }
+// ) => {
+// 	const route = context.params.route;
+// 	const body = await req.json();
+// 	if (route === "signin") {
+// 		return signin(body);
+// 	}
+// };
+
+// // ########### controllers #############
+
+// const signin = async (body: User) => {
+// 	try {
+// 		const res = await httpClient.post(`/auth/sign-in`, body);
+// 		const { accessToken } = res.data;
+// 		cookies().set(ACCESS_TOKEN_KEY, accessToken, {
+// 			httpOnly: true,
+// 			secure: process.env.NODE_ENV !== "development",
+// 			sameSite: "strict",
+// 			path: "/",
+// 		});
+// 		return NextResponse.json(res.data);
+// 	} catch (error) {
+// 		console.log(error);
+// 		return NextResponse.json({ message: "no" });
+// 	}
+// };
+
+// const signout = async (req: NextRequest) => {
+// 	try {
+// 		const cookiesStore = cookies();
+// 		cookiesStore.delete(ACCESS_TOKEN_KEY);
+// 		return NextResponse.json({ message: "ok" });
+// 	} catch (error) {
+// 		return NextResponse.json({ message: "no" });
+// 	}
+// };
+
+// const getSession = async (req: NextRequest) => {
+// 	try {
+// 		const cookiesStore = cookies();
+// 		const accessTokenKey = cookiesStore.get(ACCESS_TOKEN_KEY);
+// 		if (!!accessTokenKey?.value) {
+// 			const res = await httpClient.get(`/auth/profile`, {
+// 				headers: {
+// 					Authorization: `Bearer ${accessTokenKey?.value}`,
+// 				},
+// 			});
+
+// 			return NextResponse.json(res.data);
+// 		} else {
+// 			return NextResponse.json({ message: "no" });
+// 		}
+// 	} catch (error) {
+// 		return NextResponse.json({ message: "no" });
+// 	}
+// };
+
 import { User } from "@/src/interfaces/auth.interface";
 import { ACCESS_TOKEN_KEY } from "@/src/utils/constant";
 import httpClient from "@/src/utils/httpClient";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
+import Cors from "cors";
+
+// Initialize CORS middleware
+const cors = Cors({
+	methods: ["GET", "POST", "HEAD"],
+	origin: "https://niyom-app-web.vercel.app",
+});
+
+// Helper method to wait for a middleware to execute before continuing
+function runMiddleware(req: NextRequest, fn: Function) {
+	return new Promise((resolve, reject) => {
+		fn(req, (result: any) => {
+			if (result instanceof Error) {
+				return reject(result);
+			}
+			return resolve(result);
+		});
+	});
+}
 
 // ############# Routes #############
 // GET
@@ -15,6 +120,7 @@ export async function GET(
 	}
 ) {
 	const route = context.params.route;
+	await runMiddleware(req, cors); // Run CORS middleware
 	if (route === "signout") {
 		return signout(req);
 	} else if (route === "session") {
@@ -29,6 +135,7 @@ export const POST = async (
 	context: { params: { route: string } }
 ) => {
 	const route = context.params.route;
+	await runMiddleware(req, cors); // Run CORS middleware
 	const body = await req.json();
 	if (route === "signin") {
 		return signin(body);
